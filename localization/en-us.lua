863f3d85ec1648cd0d20685bb8d4948f6c6907f2
joshi-13 <starkjjm@gmail.com>
2026-06-10 21:12:23 +0200

    deck

---
+++ localization/en-us.lua
@@ -0,0 +1,12 @@
+return {
+    descriptions = {
+        Back = {
+            b_JoDe_tarrot = {
+                name = "Tarrot Deck",
+                text = {
+                    "deck",
+                }
+            }
+        }
+    }
+}
