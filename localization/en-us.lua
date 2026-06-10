9ec332b1c618d283f0d9c2e145261f82db0da9f5
joshi-13 <starkjjm@gmail.com>
2026-06-10 23:05:10 +0200

    1st deck description1

--- localization/en-us.lua
+++ localization/en-us.lua
@@ -1,11 +1,19 @@
 return {
     descriptions = {
         Back = {
+            b_JoDe_joker = {
+                name = "Joker Deck",
+                text = {
+                    "Only Jokers will ",
+                    "appear in the shop",
+                    
+                }
+            },
             b_JoDe_tarrot = {
                 name = "Tarrot Deck",
                 text = {
-                    "deck",
-                    "test"
+                    "Only {C:tarot}Tarot{} cards",
+                    "will appear in the shop",
                 }
             }
         }
