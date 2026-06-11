29d716e27cfb2ea54cbf3178b033c75a3138d638
joshi-13 <starkjjm@gmail.com>
2026-06-11 04:26:04 +0200

    The Basic 4 decks

--- localization/en-us.lua
+++ localization/en-us.lua
@@ -6,7 +6,7 @@ return {
                 text = {
                     "Only Jokers will ",
                     "appear in the shop",
-                    
+
                 }
             },
             b_JoDe_tarrot = {
@@ -15,7 +15,23 @@ return {
                     "Only {C:tarot}Tarot{} cards",
                     "will appear in the shop",
                 }
+            },
+            b_JoDe_spectral = {
+                name = "Spectral Deck",
+                text = {
+                    "Only {C:spectral}Spectral{} cards",
+                    "will appear in the shop",
+                }
+            },
+            b_JoDe_cards = {
+                name = "Card Deck",
+                text = {
+                    "Only playing cards will appear in the shop",
+                    "start with the {C:tarot,T:v_magic_trick}Magic Trick{}",
+                    "{C:tarot,T:v_illusion}Illusion{} vouchers",
+                }
             }
+
         }
     }
 }
